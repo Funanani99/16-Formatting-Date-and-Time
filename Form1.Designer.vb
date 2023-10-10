37dbf8749a04cea9b79521547fedb5f35e975843
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:33:27 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        LinkLabel1 = New LinkLabel()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(291, 368)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(216, 48)
+        Button1.TabIndex = 0
+        Button1.Text = "Display Date and Time"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(431, 248)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(287, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(431, 188)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(287, 31)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(431, 129)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(287, 31)
+        TextBox3.TabIndex = 3
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(431, 66)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(287, 31)
+        TextBox4.TabIndex = 4
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(431, 301)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(287, 31)
+        TextBox5.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(312, 66)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(113, 25)
+        Label1.TabIndex = 6
+        Label1.Text = "General Date"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(331, 135)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(94, 25)
+        Label2.TabIndex = 7
+        Label2.Text = "Long Date"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(328, 188)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(97, 25)
+        Label3.TabIndex = 8
+        Label3.Text = "Short Date"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(328, 251)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(95, 25)
+        Label4.TabIndex = 9
+        Label4.Text = "Long Time"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(356, 279)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(0, 25)
+        LinkLabel1.TabIndex = 10
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(325, 304)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(98, 25)
+        Label5.TabIndex = 11
+        Label5.Text = "Short Time"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label5)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox4.Text = Format(Now, "General Date")
+        TextBox3.Text = Format(Now, "Long Date")
+        TextBox2.Text = Format(Now, "short Date")
+        TextBox1.Text = Format(Now, "Long Time")
+
+        TextBox5.Text = Format(Now, "Short Time")
+    End Sub
+
+End Class
